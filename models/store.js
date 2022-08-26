901fd67592564f17e398494d6dcfd15aac683e7d
santiago <santi.prieto1212@gmail.com>
2022-08-25 23:51:43 -0500

    add required params

--- models/store.js
+++ models/store.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const StoreSchema = new mongoose.Schema({
-  name: String,
-  cuit: String,
-  concepts: Array,
-  currentBalance: Number,
-  active: Boolean,
-  lastSale: Date,
+  name: { type: String, required: true },
+  cuit: { type: String, required: true },
+  concepts: { type: Array, required: true },
+  currentBalance: { type: Number, required: true },
+  active: { type: Boolean, required: true },
+  lastSale: { type: Date, required: true },
 },{ timestamps: true });
 
 StoreSchema.pre('save', async function (callback) {
