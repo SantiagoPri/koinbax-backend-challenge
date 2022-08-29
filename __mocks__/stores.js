5d6fd70830cbc916ee9f163cbd3f817f8437a9a2
santiago <santi.prieto1212@gmail.com>
2022-08-29 11:49:20 -0500

    api unit test

---
+++ __mocks__/stores.js
@@ -0,0 +1,17 @@
+const mockedSuccesfullStore = {
+  Comercio: "La gran tienda",
+  CUIT: " 33-52652082-9",
+  "Balance actual": "$300",
+  Activo: "Si",
+  "Ultima venta": "2022-3-3",
+  "Concepto 1": "Juegos",
+  "Concepto 2": "Viajes",
+  "Concepto 3": "Panaderia",
+  "Concepto 4": "Transporte",
+  "Concepto 5": "Turismo",
+  "Concepto 6": "Ferreteria",
+};
+
+module.exports = {
+  mockedSuccesfullStore,
+};
---
+++ __test__/routes/stores.test.js
@@ -0,0 +1,109 @@
+const request = require("supertest");
+const mongoose = require("mongoose");
+const app = require("../../api/app.js");
+const { mockedSuccesfullStore } = require("../../__mocks__/stores.js");
+// const services = require("../../services/stores.js");
+const populateStores = require("../../seeders/importStores");
+const clearStores = require("../../seeders/clearStores.js");
+
+describe("Api routes for stores", () => {
+  const userName = "test@koibanx.com";
+  const password = "admin";
+
+  const server = request(app);
+  beforeAll(async () => {
+    await mongoose.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
+
+  afterEach(async () => {
+    // Clear database after each test
+    await clearStores();
+  });
+
+  describe("GET api/stores", () => {
+    //const getStoresBypageFn = jest.spyOn(services, "getStoresByPage");
+
+    it("Gets Unathorized response", async () => {
+      const response = await request(app)
+        .get("/api/stores?q={'page':2,'limit':2}")
+        .send();
+      expect(response.status).toBe(401);
+      expect(response.text).toBe("Unauthorized");
+    });
+
+    it("Works when sending page and limit", async () => {
+      //   getStoresBypageFn.getMockImplementation(
+      //     async () => mockedSuccesfullStore
+      //   );
+
+      // Populate the database for the Happy path
+      await populateStores();
+
+      const response = await server
+        .get('/api/stores?q={"page":2,"limit":2}')
+        .auth(userName, password)
+        .send();
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("data");
+      expect(response.body).toHaveProperty("page");
+      expect(response.body).toHaveProperty("pages");
+      expect(response.body).toHaveProperty("limit");
+      expect(response.body).toHaveProperty("total");
+    });
+
+    it("Doesn't works when not sending page and limit", async () => {
+      const response = await server
+        .get("/api/stores")
+        .auth(userName, password)
+        .send();
+
+      expect(response.status).toBe(500);
+      expect(response.text).toContain("internalError");
+    });
+
+    it("Doesn't works when sending incorrect page and limit", async () => {
+      const response = await server
+        .get('/api/stores?q={"page":2,"limit":2}')
+        .auth(userName, password)
+        .send();
+
+      expect(response.status).toBe(400);
+      expect(response.text).toContain("invalidParameter:");
+    });
+  });
+
+  describe("POST api/stores", () => {
+    it("Gets Unathorized response", async () => {
+      const response = await request(app).post("/api/stores").send();
+      expect(response.status).toBe(401);
+      expect(response.text).toBe("Unauthorized");
+    });
+
+    it("Works when sending the full information of a store", async () => {
+      const response = await server
+        .post("/api/stores")
+        .auth(userName, password)
+        .send(mockedSuccesfullStore);
+      expect(response.status).toBe(200);
+      expect(response.text).toBe("Store created succesfully");
+    });
+
+    it("Doesn't works when not sending body", async () => {
+      const response = await server
+        .post("/api/stores")
+        .auth(userName, password)
+        .send();
+
+      expect(response.status).toBe(400);
+      expect(response.text).toContain("invalidParameter:");
+    });
+  });
+
+  afterAll(async () => {
+    // Closing the DB connection allows Jest to exit successfully.
+    await mongoose.disconnect();
+  });
+});
